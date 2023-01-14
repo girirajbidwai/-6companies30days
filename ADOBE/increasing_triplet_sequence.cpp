404ed5f7e1b72cebc80ebe833dd430f9b21852cc
girirajbidwai <95566572+girirajbidwai@users.noreply.github.com>
2023-01-14 11:12:07 +0530

    Create increasing_triplet_sequence.cpp

---
+++ ADOBE/increasing_triplet_sequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int left = INT_MAX;
+        int mid = INT_MAX;
+
+        for (int i = 0 ; i < nums.size() ; i++) {
+            if (mid < nums[i]) {
+                return true;
+            } else if (left < nums[i] && mid > nums[i]) {
+                mid = nums[i];
+            } else if (left > nums[i]) {
+                left = nums[i];
+            }
+        }
+
+        return false;
+    }
+};
